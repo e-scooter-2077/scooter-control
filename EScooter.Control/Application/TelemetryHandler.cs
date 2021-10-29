8a3b43f5a83d9688a9a51993d04c4cf977bd3396
Simone <Simone.magnani4@studio.unibo.it>
2021-10-29 11:40:03 +0200

    Telemetries are now received from EventGrid

--- EScooter.Control/Application/TelemetryHandler.cs
+++ EScooter.Control/Application/TelemetryHandler.cs
@@ -1,43 +1,53 @@
-﻿using EScooter.Control.Web;
+﻿using System.Threading.Tasks;
+using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
+using EScooter.Control.Web;
 using Microsoft.Azure.Functions.Worker;
-using Newtonsoft.Json;
 using ScooterControlService.LogicControl.Domain;
 using ScooterControlService.LogicControl.Domain.Values;
-using System;
-using System.Threading.Tasks;
+using EventGridTriggerAttribute = Microsoft.Azure.Functions.Worker.EventGridTriggerAttribute;
 
 namespace EScooter.Control.Application
 {
+    /// <summary>
+    /// A class containing a function to handle device telemetry updates.
+    /// </summary>
     public class TelemetryHandler
     {
         private readonly IIotHubRegistryManager _iotHub;
 
+        /// <summary>
+        /// Constructor for TelemetryHandler.
+        /// </summary>
+        /// <param name="iotHub">The manager of IotHub.</param>
         public TelemetryHandler(IIotHubRegistryManager iotHub)
         {
             _iotHub = iotHub;
         }
 
+        /// <summary>
+        /// A function that when receiving an input event, submits to IotHub the new scooter.
+        /// </summary>
+        /// <param name="telemetry">The event containing the telemetry data.</param>
+        /// <param name="context">The context of the function.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
         [Function("UpdateOnNewTelemetry")]
-        public async Task UpdateOnNewTelemetry([ServiceBusTrigger("%TopicName%", "%TelemetrySub%", Connection = "ServiceBusConnectionString")] string json, FunctionContext context)
+        public async Task UpdateOnNewTelemetry([EventGridTrigger] TelemetryEvent telemetry, FunctionContext context)
         {
-            var telemetryReceived = TelemetryFromJson(json);
+            var telemetryDto = telemetry.Data;
 
             // TODO check if scooter should update, then if true:
             var scooter = new Scooter(
-                telemetryReceived.Id,
-                telemetryReceived.Tag.Locked,
-                telemetryReceived.Tag.Status with
+                telemetryDto.Id,
+                telemetryDto.Tag.Locked,
+                telemetryDto.Tag.Status with
                 {
                     BatteryLevel = BatteryLevel.FromFraction(
-                        Fraction.FromPercentage(telemetryReceived.BatteryLevel)),
-                    IsInStandby = telemetryReceived.Standby
+                        Fraction.FromPercentage(telemetryDto.BatteryLevel)),
+                    IsInStandby = telemetryDto.Standby
                 });
             await _iotHub.SubmitScooterStatus(scooter);
         }
 
-        private ScooterTelemetryDto TelemetryFromJson(string telemetryJson)
-        {
-            return JsonConvert.DeserializeObject<ScooterTelemetryDto>(telemetryJson);
-        }
+        public record TelemetryEvent(string Id, string Topic, string Subject, string EventType, ScooterTelemetryDto Data) : ExternalEvent;
     }
 }

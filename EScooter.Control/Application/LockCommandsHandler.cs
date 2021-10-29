729d56a2c09118aee9d89e8be32bf69abce56a40
Simone <Simone.magnani4@studio.unibo.it>
2021-10-29 11:40:15 +0200

    Add comments to code

--- EScooter.Control/Application/LockCommandsHandler.cs
+++ EScooter.Control/Application/LockCommandsHandler.cs
@@ -6,15 +6,28 @@ using System.Threading.Tasks;
 
 namespace EScooter.Control.Application
 {
+    /// <summary>
+    /// A class containing a function to handle lock/unlock command.
+    /// </summary>
     public class LockCommandsHandler
     {
         private readonly IIotHubRegistryManager _iotHub;
 
+        /// <summary>
+        /// Constructor for LockCommandsHandler.
+        /// </summary>
+        /// <param name="iotHub">The manager of IotHub.</param>
         public LockCommandsHandler(IIotHubRegistryManager iotHub)
         {
             _iotHub = iotHub;
         }
 
+        /// <summary>
+        /// A function that when receiving the lock command, locks the scooter and updates it on IotHub.
+        /// </summary>
+        /// <param name="json">The json containing the command data.</param>
+        /// <param name="context">The context of the function.</param>
+        /// <returns><see cref="Task"/>representing the asynchronous operation.</returns>
         [Function("LockScooter")]
         public async Task LockScooter([ServiceBusTrigger("%TopicName%", "%LockSub%", Connection = "ServiceBusConnectionString")] string json, FunctionContext context)
         {
@@ -23,6 +36,12 @@ namespace EScooter.Control.Application
             await _iotHub.SubmitScooterStatus(scooter);
         }
 
+        /// <summary>
+        /// A function that when receiving the unlock command, unlocks the scooter and updates it on IotHub.
+        /// </summary>
+        /// <param name="json">The json containing the command data.</param>
+        /// <param name="context">The context of the function.</param>
+        /// <returns><see cref="Task"/>representing the asynchronous operation.</returns>
         [Function("UnlockScooter")]
         public async Task UnlockScooter([ServiceBusTrigger("%TopicName%", "%UnlockSub%", Connection = "ServiceBusConnectionString")] string json, FunctionContext context)
         {

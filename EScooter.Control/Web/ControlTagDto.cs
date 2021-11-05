91a133a3560d36ea0996f520c4731185ea3b45cf
Simone <Simone.magnani4@studio.unibo.it>
2021-11-05 14:49:13 +0100

    Rename from scooterTag to ControlTagDto

---
+++ EScooter.Control/Web/ControlTagDto.cs
@@ -0,0 +1,15 @@
+﻿using ScooterControlService.LogicControl.Domain;
+using System;
+
+namespace EScooter.Control.Application
+{
+    public record ControlTagDto(
+        bool? Locked,
+        double? PowerSavingMaxSpeed,
+        double? PowerSavingThreshold,
+        double? DesiredMaxSpeed,
+        bool? IsInStandby,
+        double? BatteryLevel);
+
+    public record TagDto(ControlTagDto Control);
+}

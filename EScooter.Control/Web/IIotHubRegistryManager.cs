366abf5db4b3ee8d66cd98381b5586e9b0dec7aa
Simone <Simone.magnani4@studio.unibo.it>
2021-11-05 14:48:37 +0100

    Remove useless using

--- EScooter.Control/Web/IIotHubRegistryManager.cs
+++ EScooter.Control/Web/IIotHubRegistryManager.cs
@@ -1,8 +1,5 @@
 ﻿using ScooterControlService.LogicControl.Domain;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace EScooter.Control.Web

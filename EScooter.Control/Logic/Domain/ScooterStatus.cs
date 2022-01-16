0dc119a5260a0608b999ddf5b8ca399a18900c8f
Francesco Dente <dente.francesco1996@gmail.com>
2022-01-17 00:21:06 +0100

    Remove unused interfaces

--- EScooter.Control/Logic/Domain/ScooterStatus.cs
+++ EScooter.Control/Logic/Domain/ScooterStatus.cs
@@ -2,29 +2,11 @@
 
 namespace ScooterControlService.LogicControl.Domain
 {
-    public interface IScooterConfiguration
-    {
-        Speed PowerSavingMaxSpeed { get; }
-
-        BatteryLevel PowerSavingThreshold { get; }
-
-        Speed DesiredMaxSpeed { get; }
-
-        Duration UpdateFrequency { get; }
-    }
-
-    public interface IScooterReportedStatus
-    {
-        bool IsInStandby { get; }
-    }
-
     public record ScooterStatus(
         Speed PowerSavingMaxSpeed,
         BatteryLevel PowerSavingThreshold,
         Speed DesiredMaxSpeed,
         bool IsInStandby,
         BatteryLevel BatteryLevel,
-        Duration UpdateFrequency)
-        : IScooterConfiguration,
-        IScooterReportedStatus;
+        Duration UpdateFrequency);
 }
